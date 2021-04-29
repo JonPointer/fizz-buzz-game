9e24167ec50168ba0c6929227c7325ec59480aaf
Jon Pointer <jonpointer@msn.com>
2021-04-28 21:04:27 -0600

    Basic output working.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,4 @@
-const { Confirm } = require('enquirer');
+// const { Confirm } = require('enquirer');
 
 // Loop to output fizz buzz game
 // If # is divisable by 3, output fizz
@@ -6,16 +6,26 @@ const { Confirm } = require('enquirer');
 // If # is divisible by 3 and 5 (15), ouput fizz-buzz
 
 // Inquirer prompt to continue outputting every 10 numbers:
-const prompt = new Confirm({
-    name: 'question',
-    message: 'Continue?'
-});
+// const prompt = new Confirm({
+//     name: 'question',
+//     message: 'Continue?'
+// });
+// prompt.run()
+//     .then(answer => console.log('Answer:', answer))
+//     .catch(console.error);
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    if (i === 10) {
-        prompt.run()
-            .then(answer => console.log('Answer:', answer))
-            .catch(console.error);
+    if (((i % 3) === 0) && ((i % 5) !== 0)) {
+        console.log("fizz");
     }
+    else if (((i % 3) !== 0) && ((i % 5) === 0)) {
+        console.log("buzz");
+    }
+    else if (((i % 3) === 0) && ((i % 5) === 0)) {
+        console.log("fizz-buzz");
+    }
+    else {
+        console.log(i)
+    }
+
 }
