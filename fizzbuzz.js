9b9171b49b3cdab069f4c65eda6c9babb17666e9
Jon Pointer <jonpointer@msn.com>
2021-04-28 21:24:37 -0600

    Added inquirer prompt to ask for max number

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,31 +1,35 @@
-// const { Confirm } = require('enquirer');
+const { NumberPrompt } = require('enquirer');
 
 // Loop to output fizz buzz game
-// If # is divisable by 3, output fizz
+// If # is divisible by 3, output fizz
 // If # is divisible by 5, output buzz
-// If # is divisible by 3 and 5 (15), ouput fizz-buzz
+// If # is divisible by 3 and 5, ouput fizz-buzz
 
-// Inquirer prompt to continue outputting every 10 numbers:
-// const prompt = new Confirm({
-//     name: 'question',
-//     message: 'Continue?'
-// });
-// prompt.run()
-//     .then(answer => console.log('Answer:', answer))
-//     .catch(console.error);
+// Inquirer prompt to enter number to loop to:
 
-for (let i = 1; i <= 100; i++) {
-    if (((i % 3) === 0) && ((i % 5) !== 0)) {
-        console.log("fizz");
-    }
-    else if (((i % 3) !== 0) && ((i % 5) === 0)) {
-        console.log("buzz");
-    }
-    else if (((i % 3) === 0) && ((i % 5) === 0)) {
-        console.log("fizz-buzz");
-    }
-    else {
-        console.log(i)
-    }
+const prompt = new NumberPrompt({
+    name: 'number',
+    message: 'Please enter max number you would like output up to:'
+});
 
-}
+prompt.run()
+    .then(answer => {
+        for (let i = 1; i <= answer; i++) {
+            if (((i % 3) === 0) && ((i % 5) !== 0)) {
+                console.log("fizz");
+            }
+            else if (((i % 3) !== 0) && ((i % 5) === 0)) {
+                console.log("buzz");
+            }
+            else if (((i % 3) === 0) && ((i % 5) === 0)) {
+                console.log("fizz-buzz");
+            }
+            else {
+                console.log(i)
+            }
+
+        }
+    })
+    .catch(console.error);
+
+
